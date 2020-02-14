2cf1f5a1c14a13e4d82c78ec5941b5384818db1f
Marcus Deuss <marcus.deuss@me.com>
2020-02-14 16:17:20 +0100

    fixes

--- DailyToolbox/ConvertNumbersViewController.swift
+++ DailyToolbox/ConvertNumbersViewController.swift
@@ -26,7 +26,11 @@ class ConvertNumbersViewController: UIViewController, UITextFieldDelegate {
         
         self.title = "Convert Numbers"
         
-        self.decimalTextField.becomeFirstResponder()
+        decimalTextField.becomeFirstResponder()
+        
+        decimalTextField.delegate = self
+        hexaTextField.delegate = self
+        binaryTextField.delegate = self
         
         decimalTextField.addTarget(self, action: #selector(ConvertNumbersViewController.decimalTextFieldDidChange(_:)), for: UIControl.Event.editingChanged)
         
@@ -66,7 +70,7 @@ class ConvertNumbersViewController: UIViewController, UITextFieldDelegate {
             let input : String = textField.text!
             let num = ConvertNumbers(binary: input)
             decimalTextField.text = String(num.decimal)
-            hexaTextField.text = num.hexadecimal
+            hexaTextField.text = num.hexadecimal.uppercased()
         }
         else{
             decimalTextField.text = ""
@@ -74,6 +78,69 @@ class ConvertNumbersViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
+    // check in price textfield for comma or dot characters - somehow people change keyboard input type from decimal to text
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if textField == decimalTextField{
+            if let char = string.cString(using: String.Encoding.utf8) {
+                let isDot = strcmp(char, ".")
+                
+                if isDot == 0{
+                    return false
+                }
+                
+                let isComma = strcmp(char, ",")
+                
+                if isComma == 0{
+                    return false
+                }
+            }
+        }
+        
+        if textField == hexaTextField{
+            if let char = string.cString(using: String.Encoding.utf8) {
+               /* let isBackspace = strcmp(char, "\\b")
+                print(string)
+                if isBackspace == 0{
+                    return false
+                }
+                */
+                switch(string){
+                case "a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F":
+                    return true
+                    
+                case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+                    return true
+                
+                case "":
+                    return true
+                    
+                default:
+                        return false
+                }
+            }
+        }
+        
+        if textField == binaryTextField{
+            if let char = string.cString(using: String.Encoding.utf8) {
+                let isZero = strcmp(char, "0")
+                
+                if isZero == 0{
+                    return true
+                }
+                
+                let isOne = strcmp(char, "1")
+                
+                if isOne == 0{
+                    return true
+                }
+                
+                return false
+            }
+        }
+        
+        return true
+    }
 
     /*
     // MARK: - Navigation
