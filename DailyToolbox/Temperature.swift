d46496cd7cc1bd0c6f0a5e540274c6561fdc9e7f
Marcus Deuss <marcus.deuss@me.com>
2020-02-23 14:44:18 +0100

    some improvements like double to string conversion and temperature module

--- DailyToolbox/Temperature.swift
+++ DailyToolbox/Temperature.swift
@@ -14,15 +14,29 @@ class Temperature: CustomStringConvertible{
     // properties
     var kelvin: Double
     
+    
     // computed properties
+    
+    var kelvinToString: String{
+        return String(format: "%.2f", kelvin)
+    }
+    
     var celsius: Double{
         get { return kelvin - 273.15}
         set { kelvin = newValue + 273.15}
     }
     
+    var celsiusToString: String{
+        return String(format: "%.2f", celsius)
+    }
+    
     var fahrenheit: Double{
         get { return celsius * 1.8 + 32.0}
         set { kelvin = (newValue - 32.0) / 1.8 + 273.15}
+    }
+    
+    var fahrenheitToString: String{
+        return String(format: "%.2f", fahrenheit)
     }
     
     // Init functions
--- DailyToolbox/TemperatureViewController.swift
+++ DailyToolbox/TemperatureViewController.swift
@@ -7,21 +7,36 @@
 //
 
 import UIKit
-import WebKit
+//import WebKit
 
-class TemperatureViewController: UIViewController{
+class TemperatureViewController: UIViewController, UITextFieldDelegate{
   
-    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var celsiusTextField: UITextField!
+    @IBOutlet weak var fahrenheitTextField: UITextField!
+    @IBOutlet weak var kelvinTextField: UITextField!
+    
     
     func configureView() {
         // Update the user interface for the detail item.
         
         self.title = "Temperature Calculation"
         
-        let url = URL(string: "https://dict.leo.org/dict/mobile.php")!
+    /*    let url = URL(string: "https://dict.leo.org/dict/mobile.php")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        */
         
+        celsiusTextField.becomeFirstResponder()
+        
+        celsiusTextField.delegate = self
+        fahrenheitTextField.delegate = self
+        kelvinTextField.delegate = self
+        
+        celsiusTextField.addTarget(self, action: #selector(TemperatureViewController.celsiusTextFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        
+        fahrenheitTextField.addTarget(self, action: #selector(TemperatureViewController.fahrenheitTextFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        
+        kelvinTextField.addTarget(self, action: #selector(TemperatureViewController.kelvinTextFieldDidChange(_:)), for: UIControl.Event.editingChanged)
     }
 
     override func viewDidLoad() {
@@ -29,4 +44,88 @@ class TemperatureViewController: UIViewController{
         // Do any additional setup after loading the view.
         configureView()
     }
+    
+    @objc func celsiusTextFieldDidChange(_ textField: UITextField) {
+        if textField.text!.count > 0{
+            let input : Double = Double(textField.text!)!
+            let value = Temperature(celsius: input)
+            fahrenheitTextField.text = value.fahrenheitToString
+            kelvinTextField.text = value.kelvinToString
+        }
+        else{
+            fahrenheitTextField.text = ""
+            kelvinTextField.text = ""
+        }
+    }
+    
+    @objc func fahrenheitTextFieldDidChange(_ textField: UITextField) {
+        if textField.text!.count > 0{
+            let input : Double = Double(textField.text!)!
+            let value = Temperature(fahrenheit: input)
+            celsiusTextField.text = value.celsiusToString
+            kelvinTextField.text = value.kelvinToString
+        }
+        else{
+            celsiusTextField.text = ""
+            kelvinTextField.text = ""
+        }
+    }
+    
+    @objc func kelvinTextFieldDidChange(_ textField: UITextField) {
+        if textField.text!.count > 0{
+            let input : Double = Double(textField.text!)!
+            let value = Temperature(kelvin: input)
+            celsiusTextField.text = value.celsiusToString
+            fahrenheitTextField.text = value.fahrenheitToString
+        }
+        else{
+            celsiusTextField.text = ""
+            fahrenheitTextField.text = ""
+        }
+    }
+    
+    // check for valid keyboard input characters
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == celsiusTextField{
+            switch(string){
+            case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".":
+                return true
+            
+            case "":
+                return true
+                
+            default:
+                    return false
+            }
+        }
+        
+        if textField == fahrenheitTextField{
+            switch(string){
+            case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".":
+                return true
+            
+            case "":
+                return true
+                
+            default:
+                    return false
+            }
+        }
+        
+        if textField == kelvinTextField{
+            switch(string){
+            case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".":
+                return true
+            
+            case "":
+                return true
+                
+            default:
+                    return false
+            }
+        }
+        
+        return true
+    }
+
 }
