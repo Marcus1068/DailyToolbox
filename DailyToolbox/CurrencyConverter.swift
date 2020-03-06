3436e382ab228c8e4a2853cf8b1efd2ab516a336
Marcus Deuss <marcus.deuss@me.com>
2020-03-06 17:41:55 +0100

    refactor

--- DailyToolbox/CurrencyConverter.swift
+++ DailyToolbox/CurrencyConverter.swift
@@ -8,38 +8,103 @@
 
 import Foundation
 
-struct CurrencyCube {
+struct Cube {
     var currency: String
     var rate: String
 }
 
-class CurrencyConverter: NSObject, XMLParserDelegate {
-    var currency: String
-    var rate: String
 
+// need to be declared outside of class otherwise loses values after xml parser runs
+var cubes: [Cube] = []
+
+// works based on EUR reference currency
+
+class CurrencyConverter: NSObject, XMLParserDelegate {
+    
+    // variables
+    var elementName: String = String()
+    var currency = String()
+    var rate = String()
+
+    // init functions
     override init(){
-        currency = ""
-        rate = ""
+        super.init()
+        
+        let xmlFileURL = URL(string: "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
+        
+        let parser = XMLParser(contentsOf: xmlFileURL!)
+        let xmlDic = self
+        parser!.delegate = xmlDic
+        if parser!.parse()
+        {
+            print("XML Parsing OK")
+            print(cubes)
+            print(cubes.count)
+        }
+        else
+        {
+            print("XML Parser error: ", parser!.parserError!, ", line: ", parser!.lineNumber, ", column: ", parser!.columnNumber);
+        }
     }
     
-  /*  class func requestCurrency(completionHandler: @escaping (String?, String?, Error?) -> Void) {
-        let url = URL(string: "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")!
-        let task = URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data, error == nil else {
-                DispatchQueue.main.async {
-                    completionHandler(nil, nil, error)
-                }
-                return
-            }
-
-            let delegate = CurrencyConverter()
-            let parser = XMLParser(data: data)
-            parser.delegate = delegate
-            DispatchQueue.main.async {
-                completionHandler(delegate.currency, delegate.rate, parser.parserError)
+    
+    // functions
+    
+    // get USD currency or fail
+    func getUSDCurrency() -> Double?{
+        for v in cubes{
+            if v.currency == "USD"{
+                return Double(v.rate)!
             }
         }
-        task.resume()
-    } */
+        
+        return nil
+    }
+    
+    // delegates
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+
+        if elementName == "Cube" {
+            if let name = attributeDict["currency"]{
+                currency = name
+                //print(currency)
+            }
+            
+            if let tag = attributeDict["rate"]{
+                rate = tag
+                //print(rate)
+            }
+            
+        }
+        
+        self.elementName = elementName
+    }
+
+
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "Cube" {
+            let cube = Cube(currency: currency, rate: rate)
+            cubes.append(cube)
+            //print(cubes.count)
+        }
+    }
+
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        //let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        //print(data)
+        //print(string)
+        /*if (!data.isEmpty) {
+            if self.elementName == "Cube" {
+                currency += data
+                rate += data
+            }
+        } */
+    }
+        
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        print("XML Parser failure error: ", parseError)
+    }
+  
 }
 
