a88b8f708573c79cb7578889bb9c948f5f3fb706
Marcus Deuss <marcus.deuss@me.com>
2020-03-07 11:49:23 +0100

    more work on currency conversion

--- DailyToolbox/CurrencyConverter.swift
+++ DailyToolbox/CurrencyConverter.swift
@@ -6,6 +6,53 @@
 //  Copyright © 2020 Marcus Deuß. All rights reserved.
 //
 
+
+// uses this XML from ECB website:
+/* <gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01" xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+<gesmes:subject>Reference rates</gesmes:subject>
+<gesmes:Sender>
+<gesmes:name>European Central Bank</gesmes:name>
+</gesmes:Sender>
+<Cube>
+<Cube time="2020-03-06">
+<Cube currency="USD" rate="1.1336"/>
+<Cube currency="JPY" rate="119.08"/>
+<Cube currency="BGN" rate="1.9558"/>
+<Cube currency="CZK" rate="25.458"/>
+<Cube currency="DKK" rate="7.4697"/>
+<Cube currency="GBP" rate="0.87165"/>
+<Cube currency="HUF" rate="335.48"/>
+<Cube currency="PLN" rate="4.3042"/>
+<Cube currency="RON" rate="4.8110"/>
+<Cube currency="SEK" rate="10.6145"/>
+<Cube currency="CHF" rate="1.0589"/>
+<Cube currency="ISK" rate="143.00"/>
+<Cube currency="NOK" rate="10.4983"/>
+<Cube currency="HRK" rate="7.5035"/>
+<Cube currency="RUB" rate="77.5058"/>
+<Cube currency="TRY" rate="6.9209"/>
+<Cube currency="AUD" rate="1.7103"/>
+<Cube currency="BRL" rate="5.2748"/>
+<Cube currency="CAD" rate="1.5213"/>
+<Cube currency="CNY" rate="7.8511"/>
+<Cube currency="HKD" rate="8.8089"/>
+<Cube currency="IDR" rate="16558.49"/>
+<Cube currency="ILS" rate="3.9576"/>
+<Cube currency="INR" rate="83.5860"/>
+<Cube currency="KRW" rate="1351.63"/>
+<Cube currency="MXN" rate="22.9958"/>
+<Cube currency="MYR" rate="4.7294"/>
+<Cube currency="NZD" rate="1.7858"/>
+<Cube currency="PHP" rate="57.542"/>
+<Cube currency="SGD" rate="1.5630"/>
+<Cube currency="THB" rate="35.640"/>
+<Cube currency="ZAR" rate="17.8514"/>
+</Cube>
+</Cube>
+</gesmes:Envelope>
+ */
+
+
 import Foundation
 
 struct Cube {
@@ -15,16 +62,16 @@ struct Cube {
 
 
 // need to be declared outside of class otherwise loses values after xml parser runs
-var cubes: [Cube] = []
+private var cubes: [Cube] = []
 
 // works based on EUR reference currency
 
 class CurrencyConverter: NSObject, XMLParserDelegate {
     
     // variables
-    var elementName: String = String()
-    var currency = String()
-    var rate = String()
+    private var elementName: String = String()
+    private var currency = String()
+    private var rate = String()
 
     // init functions
     override init(){
@@ -50,6 +97,35 @@ class CurrencyConverter: NSObject, XMLParserDelegate {
     
     // functions
     
+    // list of all currencies and according rates
+    func getCurrencyList() -> [Cube]{
+        return cubes
+    }
+    
+    // list of all currencies and according rates
+    func getCurrencyStrings() -> [String]{
+        var str: [String] = []
+        
+        str.append("EUR")
+        
+        for i in cubes{
+            str.append(i.currency)
+        }
+        
+        return str
+    }
+    
+    // get rate based on currency
+    func getRate(currency: String) -> Double?{
+        for v in cubes{
+            if v.currency == currency{
+                return Double(v.rate)!
+            }
+        }
+        
+        return nil
+    }
+    
     // get USD currency or fail
     func getUSDCurrency() -> Double?{
         for v in cubes{
@@ -62,7 +138,7 @@ class CurrencyConverter: NSObject, XMLParserDelegate {
     }
     
     // delegates
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+    internal func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
 
         if elementName == "Cube" {
             if let name = attributeDict["currency"]{
@@ -81,7 +157,7 @@ class CurrencyConverter: NSObject, XMLParserDelegate {
     }
 
 
-    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+    internal func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         if elementName == "Cube" {
             let cube = Cube(currency: currency, rate: rate)
             cubes.append(cube)
@@ -90,7 +166,7 @@ class CurrencyConverter: NSObject, XMLParserDelegate {
     }
 
     
-    func parser(_ parser: XMLParser, foundCharacters string: String) {
+    internal func parser(_ parser: XMLParser, foundCharacters string: String) {
         //let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         //print(data)
         //print(string)
@@ -102,7 +178,7 @@ class CurrencyConverter: NSObject, XMLParserDelegate {
         } */
     }
         
-    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+    internal func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
         print("XML Parser failure error: ", parseError)
     }
   
--- DailyToolbox/CurrencyConverterViewController.swift
+++ DailyToolbox/CurrencyConverterViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 
-class CurrencyConverterViewController: UIViewController {
+class CurrencyConverterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let cvt = CurrencyConverter()
+    var numberOfCurrencies : Int = 0
+    var fromCurrencyList: [String] = []
+    var toCurrencyList: [String] = []
+    
+    
+    @IBOutlet weak var fromTextField: UITextField!
+    @IBOutlet weak var fromCurrencyPicker: UIPickerView!
+    @IBOutlet weak var toCurrencyPicker: UIPickerView!
+    @IBOutlet weak var toTextField: UITextField!
+    
     
     func configureView() {
         
         self.title = "Currency Converter"
         
-        let cvt = CurrencyConverter()
+        fromTextField.becomeFirstResponder()
+        fromTextField.text = "1"
+
+        numberOfCurrencies = cvt.getCurrencyStrings().count
+        
+        fromCurrencyList = cvt.getCurrencyStrings()
+        toCurrencyList = cvt.getCurrencyStrings()
+        
+        // Connect data:
+        self.fromCurrencyPicker.delegate = self
+        self.fromCurrencyPicker.dataSource = self
+        self.toCurrencyPicker.delegate = self
+        self.toCurrencyPicker.dataSource = self
+        
         if let dollar = cvt.getUSDCurrency(){
             print(dollar)
         }
         
+        let cur = cvt.getCurrencyList()
+        
+        for i in cur{
+            print("Währung: \(i.currency), Kurs: \(i.rate)")
+        }
     }
     
     override func viewDidLoad() {
@@ -30,7 +60,34 @@ class CurrencyConverterViewController: UIViewController {
         
     }
     
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        
+        return numberOfCurrencies
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        switch(pickerView){
+        case fromCurrencyPicker:
+            return fromCurrencyList[row]
+        case toCurrencyPicker:
+            return toCurrencyList[row]
+        default:
+            return fromCurrencyList[row]
+        }
+    }
+    
+    // Capture the picker view selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // This method is triggered whenever the user makes a change to the picker selection.
+        // The parameter named row and component represents what was selected.
+    }
+    
     /*
     // MARK: - Navigation
 
