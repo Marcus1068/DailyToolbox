fe80229c5a66ae5a6e3196aa6988381b5c84260c
Marcus Deuss <marcus.deuss@me.com>
2020-03-23 17:41:46 +0100

    interest rates model added

--- DailyToolbox/InterestRate.swift
+++ DailyToolbox/InterestRate.swift
@@ -27,5 +27,57 @@ limitations under the License.
 import Foundation
 
 class InterestRate{
+    // Eigenschaften
+    
+    // https://www.gut-erklaert.de/mathematik/zinsrechnung-formeln-beispiele.html
+    
+    // Der Prozentwert gibt Antwort auf Fragen wie:
+    // „Wie viele Schüler sind 25% von 24 Schülern?“ - Gesucht: Prozentwert, gegeben: Prozenzsatz (25%) und Grundwert (24 Schüler)
+    // „Wie viel sind 3% von 100 Euro?“ - Gesucht: Prozentwert, gegeben: Prozentsatz (3%) und Grundwert (100 Euro)
+    var zinsen: Double
+    
+    var zinsenToString: String{
+        return String(format: "%.2f", zinsen)
+    }
+    
+    // Der Grundwert gibt Antwort auf Fragen wie:
+    // „Wenn 6 Schüler 25% der Klasse sind, wie viele Schüler hat dann die ganze Klasse?“
+    // „Wenn Sie beim Kauf eines Autos 500€ Rabatt bekommen haben und dies 15% Rabatt auf den Kaufpreis entspricht, wie hoch war dann der Kaufpreis für das Auto?“
+    var kapital: Double
+    
+    var kapitalToString: String{
+        return String(format: "%.2f", kapital)
+    }
+    
+    // Der Prozentsatz gibt Antwort auf Fragen wie:
+    // „Wie viel Prozent machen 6 Schüler von 24 Schülern aus?“
+    // „Wenn der Gewinn eines Unternehmens im Vorjahr 10000€ betrug und der Gewinn dieses Jahr bei 12000€ liegt, wie viel Prozent Gewinn hat das Unternehmen erwirtschaftet?“
+    // prozentsatz = prozentwert/grundwert * 100%
+    var zinssatz: Double
+    
+    var zinssatzToString: String{
+        return String(format: "%.2f", zinssatz)
+    }
+    
+    // Kapital ermitteln
+    init(zinsen: Double, zinssatz: Double){
+        self.zinsen = zinsen
+        self.zinssatz = zinssatz
+        self.kapital = zinsen / zinssatz * 100.0
+    }
+    
+    // Zinssatz in % ermitteln
+    init(zinsen: Double, kapital: Double){
+        self.zinsen = zinsen
+        self.kapital = kapital
+        self.zinssatz = zinsen / kapital * 100.0
+    }
+    
+    // Zinswert ermitteln
+    init(zinssatz: Double, kapital: Double){
+        self.zinssatz = zinssatz
+        self.kapital = kapital
+        self.zinsen = zinssatz * kapital / 100.0
+    }
     
 }
